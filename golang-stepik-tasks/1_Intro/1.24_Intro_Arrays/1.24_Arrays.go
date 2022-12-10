4b54e8537418b6f61e7bb998e47d8e8377c8d0c8
Alex Dvornik <aadvornik@gmail.com>
2022-12-10 23:51:30 +0300

    Resolved task 24

--- golang-stepik-tasks/1_Intro/1.24_Intro_Arrays/1.24_Arrays.go
+++ golang-stepik-tasks/1_Intro/1.24_Intro_Arrays/1.24_Arrays.go
@@ -4,9 +4,16 @@ import "fmt"
 
 
 func main() {
-     var a int		
-     for i:=10; i!=0; i--{
-          fmt.Scan(&a)
+     var a, b, c int
+     var workArray = [10]uint8{}
+     for i := 0; i != 10; i++ {
+	  fmt.Scan(&a)
+	  workArray[i] = uint8(a)  
      }
-}
 
+     for i := 0; i < 3; i++ {
+     	  fmt.Scan(&b, &c)
+          workArray[b], workArray[c] = workArray[c], workArray[b]
+     }
+     fmt.Println(workArray)
+}     

245c114bda789fab76fe177775b6e470bec7950e
Alex Dvornik <aadvornik@gmail.com>
2023-01-28 19:57:41 +0300

    resolved 1.42

---
+++ golang-stepik-tasks/1_Intro/1.43_Intro_Recap/1.43_recap.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+    var value, to_find int
+    result := make([]int, 0)
+    
+    fmt.Scan(&value)
+    fmt.Scan(&to_find)
+
+    for ; value > 0 ; {
+        last := value % 10
+	if last != to_find {
+            result = append(result, last) 
+	}
+	value /= 10
+    }
+    for i:=len(result)-1; i>=0; i-- {
+        fmt.Print(result[i])
+    }
+}
+

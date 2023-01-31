9a01c9808fa2f3c7418782a5c8812e6487c0d800
Alex Dvornik <aadvornik@gmail.com>
2023-01-31 11:55:50 +0300

    updated 2.1.4

--- golang-stepik-tasks/2_Functions_structures_pointers/2.1.4_Functions/2.1.4_func.go
+++ golang-stepik-tasks/2_Functions_structures_pointers/2.1.4_Functions/2.1.4_func.go
@@ -10,10 +10,8 @@ func main() {
 
 func fibonacci(n int) int {
 	var fib1, fib2 = 1, 1
-	arr := []int{1, 1}
-	for i := 0; i < n; i++ {
+	for i := 1; i < n; i++ {
 		fib1, fib2 = fib2, fib2+fib1
-		arr = append(arr, fib2)
 	}
-	return arr[len(arr)-3]
+	return fib1
 }
